3c0f53226bb8d1eba1fc3a203cd51a9753b1daa2
Tobias Scholze <t.scholze@anfe.ma>
2018-11-06 08:56:55 +0100

    - Fixed unhappy clean up

--- frettir/frettir/ViewModels/SettingsViewModel.cs
+++ frettir/frettir/ViewModels/SettingsViewModel.cs
@@ -32,7 +32,6 @@ namespace frettir.ViewModels
             }
 
             // Process valid blog feed
-            MessagingCenter.Send<SettingsViewModel>(this, Constants.NOTIFICATION_ID_ADDFEED_SUCCESS);
             MessagingCenter.Send<SettingsViewModel, string>(this, Constants.NOTIFICATION_ID_ADDTABITEM, urlString);
         }
     }

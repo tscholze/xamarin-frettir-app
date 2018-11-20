d5bfa5f1a7b35cab2e09073117af7632cdaa154a
Tobias Scholze <t.scholze@anfe.ma>
2018-11-20 13:57:04 +0100

    - Fixed settings icon asset - Added custom NavigationPage with additional features

---
+++ frettir/frettir/Controls/FRNavigationPage.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using XF.Material.Forms.UI;
+using Xamarin.Forms;
+
+namespace frettir.Controls
+{
+    /// <summary>
+    /// Custom subclass of `MaterialNavigationPage`. That is enriched with additional features.
+    /// </summary>
+    public class FRNavigationPage : MaterialNavigationPage
+    {
+        #region Init 
+
+        public FRNavigationPage(Page rootPage) : base(rootPage)
+        {
+            SetupToolbarItems();
+        }
+
+        #endregion
+
+        #region Private helper
+
+        /// <summary>
+        /// Setups the always visisble but customized toolbar items.
+        /// </summary>
+        void SetupToolbarItems()
+        {
+            var settings = new ToolbarItem
+            {
+                Icon = "icon_settings.png"
+            };
+
+            ToolbarItems.Add(settings);
+        }
+
+        #endregion
+    }
+}
